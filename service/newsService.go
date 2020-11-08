ef311e8f5312dab3fd8fe996aebad0ed8fc120f0
Bekarys <bekarysmoldakhmetov@gmail.com>
2020-11-09 04:03:34 +0600

    Initial

--- service/newsService.go
+++ service/newsService.go
@@ -11,6 +11,7 @@ type NewsService struct { }
 func (NewsService) AddNews(news *model.News) error {
 	_, err := database.GetInstance().NamedExec("INSERT INTO news(title, author, text, category_id) " +
 																	 "SELECT :title, :author, :text, id FROM categories WHERE name ~* :category", &news);
+	// TODO: valdiate updated/insertd rows count
 	if err != nil {
 		return errors.New("Something went wrong")
 	}
